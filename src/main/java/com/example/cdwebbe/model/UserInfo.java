17ad6c780465152d32d0b7df272702fe918f7c72
nickffcom <nickffcom@gmail.com>
2022-06-08 17:13:23 +0700

    Hoàn thành jwt,Login,Register,Update profile

---
+++ src/main/java/com/example/cdwebbe/model/UserInfo.java
@@ -0,0 +1,94 @@
+package com.example.cdwebbe.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserInfo {
+    private Long id;
+    private String username;
+    private String name;
+    private String gender;
+    private String address;
+    private String phone;
+
+    private String email;
+
+    private Set<Role> roles = new HashSet<>();
+
+    public UserInfo(Long id, String username, String name, String gender, String address, String phone,String email, Set<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.gender = gender;
+        this.address = address;
+        this.phone = phone;
+        this.email=email;
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
